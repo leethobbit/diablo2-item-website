44ae49bfb957735d269fffc87d0dda4c567cd680
Rink Springer <rink@rink.nu>
2019-02-16 11:49:13 +0100

    Add docker-compose setup
    This may be a bit hacky, but it gets the job done.

---
+++ www/config.php
@@ -0,0 +1,7 @@
+<?php
+    // This configuration is intended for the docker-compose setup
+    // Refer to config.sample.php for a template configuration
+	define (DB_DSN, "pgsql://diablo2:diablo2@database/diablo2");
+	define (TEMPLATE_PATH, "/templates");
+	define (COMPILE_PATH, "/tmp/compiled");
+ ?>
